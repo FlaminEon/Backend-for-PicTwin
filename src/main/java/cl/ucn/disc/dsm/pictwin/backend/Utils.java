b60780f449c43c29134a1a637e3e9c80fdd31fa1
FlaminEon <aeonf4@gmail.com>
2022-07-16 01:13:02 -0400

    style: class now complies with coding standards

--- src/main/java/cl/ucn/disc/dsm/pictwin/backend/Utils.java
+++ src/main/java/cl/ucn/disc/dsm/pictwin/backend/Utils.java
@@ -23,22 +23,23 @@ import org.apache.commons.lang3.builder.RecursiveToStringStyle;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
- * The Utils
+ * The Utils.
  *
- * @author Cross
+ * @author Cross.
  */
 @Slf4j
 public final class Utils {
 
     /**
-     * Print in log the internal of one object
+     * Print in log the internal of one object.
      *
-     * @param objectName to print
-     * @param o to print
+     * @param objectName to print.
+     * @param o to print.
      */
-    public static void printObject(@NonNull String objectName, Object o){
-        if (o != null){
-            log.debug("{}: {}", objectName, ToStringBuilder.reflectionToString(o, RecursiveToStringStyle.MULTI_LINE_STYLE));
+    public static void printObject(@NonNull String objectName, Object o) {
+        if (o != null) {
+            log.debug("{}: {}", objectName, ToStringBuilder.reflectionToString(
+                    o, RecursiveToStringStyle.MULTI_LINE_STYLE));
         } else {
             log.debug("{}: {}", objectName, null);
         }
